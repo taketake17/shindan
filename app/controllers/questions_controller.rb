79a258633dbc2c3eb14c2bcc851dd209148e02a4
梅木健 <umeki@MacBook-Air.local>
2025-02-16 15:29:45 +0900

    途中経過（showアクションの細分化）

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,27 +9,15 @@ class QuestionsController < ApplicationController
 
     def show
         @question = Question.find(params[:id])
-        @result = case (1..4).map { |i| @question.send("question#{i}") }.map(&:to_i).sum
-        when 12
-            "マジで言ってます？良い人狙いに行ってませんか？"
-        when 10..11
-            "あなたはとても良い人だ！素晴らしい！"
-        when 8..9
-            "あなたは良い人だ！自信持って！"
-        when 6..7
-            "あなたはまあまあ良い人だ！これくらいの方が人間味があって良いよね"
-        when 4..5
-            "もっと周りに優しくしてあげても良いかもね"
-        when 1..3
-            "ちょっと心配になるかも。。。。"
-        when 0
-            "まさに悪童だ！怖い！"
-        end
+        @result = calculate_result(@question)
+        @evaluation = evaluate_result(@result)
     end
 
     def create
         @question = Question.new(question_params)
         if @question.save
+            session[:result] = @result
+            session[:evaluation] = @evaluation
             flash[:notice] = "診断が完了しました"
             redirect_to question_path(@question)
         else
@@ -38,6 +26,29 @@ class QuestionsController < ApplicationController
     end
 
     private
+
+    def calculate_result(question)
+       (1..4).map { |i| question.send("question#{i}") }.map(&:to_i).sum
+    end
+
+    def evaluate_result(result)
+        case result 
+        when 12
+            "マジで言ってます？良い人狙いに行ってませんか？"
+        when 10..11
+            "とても良い人だ！素晴らしい！"
+        when 8..9
+            "良い人だ！自信持って！"    
+        when 6..7
+            "まあまあ良い人だ！これくらいの方が人間味があって良いよね"
+        when 4..5
+            "もっと周りに優しくしてあげても良いかもね"
+        when 1..3
+            "ちょっと心配になるかも。。。。"    
+        when 0
+            "まさに悪童だ！逃げろ！"
+        end
+    end 
 
     def question_params
         params.require(:question).permit(:question1, :question2, :question3, :question4)
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+    validates :question1, presence: true
+    validates :question2, presence: true
+    validates :question3, presence: true
+    validates :question4, presence: true
 end
